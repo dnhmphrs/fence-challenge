68e34f9c66fb6c44372c3ee369708720bc1a5839
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-12-20 16:52:09 +0100

    edit

--- src/lib/graphics/scene.svelte
+++ src/lib/graphics/scene.svelte
@@ -47,7 +47,7 @@ let pentominosDict = {
 			letter: 'P',
 			width: 3,
 			height: 3,
-			cornerVertices: [[0, 2], [2, 2], [2, 1], [1, 1], [1, 0], [0, 0]]
+			cornerVertices: [[-1, -1], [-1, 0], [0, 0], [0, 1], [2, 1], [2, -1], [-1, -1]]
 		},
 		5: {
 			letter: 'T',
