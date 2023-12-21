bc4ca8d6d35cae438aba3ed9f1835e4afaa7e8da
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-12-21 14:26:57 +0100

    edit adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from 'svelte-preprocess';
-// import adapter from '@sveltejs/adapter-netlify';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,11 +11,8 @@ const config = {
 	],
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			edge: false,
+			split: true
 		})
 	},
 	compilerOptions: {
