621ff93cc77c8367e66dbe74456d0d294d20a236
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2024-01-03 12:09:42 +0000

    Initial Rebuild

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,101 @@
 <script>
-	import '../app.css';
+	import './styles.css';
+
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+	import { webVitals } from '$lib/vitals';
+
+	import { onMount } from 'svelte';
+	import { screenType, isIframe, screenSize } from '$lib/store/store';
+	import { getDeviceType, getScreenSize } from '$lib/functions/utils';
+
+	export let data;
+	let Geometry;
+
+	$: if (browser && data?.analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId: data.analyticsId
+		});
+	}
+
+	function handleScreen() {
+		// screen size
+		screenSize.set(getScreenSize());
+
+		// device type
+		screenType.set(getDeviceType());
+		isIframe.set(window.location !== window.parent.location);
+	} 
+
+
+	onMount(async () => {
+		// webgl
+		const module = await import('$lib/graphics/webgl.svelte');
+    Geometry = module.default;
+
+		handleScreen();
+		window.addEventListener('resize', () => handleScreen());
+
+		return () => {
+			window.removeEventListener('resize', () => handleScreen());
+		};
+	});
 </script>
 
-<slot />
+<svelte:head>
+	<title>Fence Challenge</title>
+	<meta name="description" content="WIP" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="AUFBAU" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</svelte:head>
+
+{#if Geometry}
+    <svelte:component this={Geometry} />
+{:else}
+    <div class="loading">loading.</div>
+{/if}
+
+<div class="app">
+
+	<main>
+		<slot />
+	</main>
+
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100dvh;
+		width: 100%;
+	}
+
+	.loading {
+		position: absolute;
+		font-style: italic;
+		font-family: serif;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		padding: 10px;
+		font-size: 12px;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 1rem;
+		width: 100%;
+		height: 100%;
+	}
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-})
+	plugins: [sveltekit(), glsl()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
