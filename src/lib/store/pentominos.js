0e7916172b7befa8b9fa3cf80e4dd7b14101862e
Miguel O'Malley <miguelomalley9@gmail.com>
2024-02-23 14:00:49 +0100

    Add files via upload

--- src/lib/store/pentominos.js
+++ src/lib/store/pentominos.js
@@ -1,3 +1,5 @@
 import { writable } from 'svelte/store';
 
 export const selectedPentominos = writable([]);
+export const toRotatePentominos = writable(0);
+export const toFlipPentominos = writable(0);
