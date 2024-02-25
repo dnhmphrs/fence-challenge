57bdaf8a9aa5735535240a70904d575a579dabb4
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2024-02-25 22:19:36 +0000

    brun changes

--- src/lib/functions/pyodide.js
+++ src/lib/functions/pyodide.js
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
+import { pyodideSays } from '$lib/store/pentominos';
+=======
 import { pArea, pIDs, pFencedTiles } from "$lib/store/pentominos";
 import {pentominosKey} from "$lib/graphics/pentominos";
+>>>>>>> 9a1f3571b1403af032483cc1bcaf5a09a5a8b18a
 // load pyodide on site load
 export async function handleLoadPyodide() {
 	window.pyodide = await window.loadPyodide();
@@ -88,24 +92,11 @@ export async function processBoard(
 
 					result
 			`);
-			console.log(`${result}`);
-			alert(`Result from Python: ${result}`);
-			let pOut = pyodide.globals.get('result').toJs();
-			console.log(pOut);
-			//pArea.set(pOut.get('area'));
-			pArea.set(pyodide.globals.get('area'));
-			//pFencedTiles.set(pOut.get('fencedTiles'));
-			pFencedTiles.set(pyodide.globals.get('fencedAreas').toJs());
-			let pIDNums = pOut.get('id');
-			let pIDLets = '';
-			for (let i = 0; i<pIDNums.length; i++)
-			{
-				pIDLets += pentominosKey[pIDNums[i]];
-			}
-			pIDs.set(pIDLets);
-		} catch (error) {
-			console.error('Error in processBoard:', error);
-			alert(`Result from Python: ${error}`);
-		}
+		console.log(`${result}`);
+		alert(`Result from Python: ${result}`);
+		pyodideSays.set(window.pyodide.globals.get('result').toJs());
+	} catch (error) {
+		console.error('Error in processBoard:', error);
+		alert(`Result from Python: ${error}`);
 	}
 }
