d6f79c39d00338457121bff829e38a4ae8c436e3
Miguel O'Malley <miguelomalley9@gmail.com>
2024-02-26 07:41:41 +0100

    conflict resolution

--- src/lib/store/pentominos.js
+++ src/lib/store/pentominos.js
@@ -5,4 +5,8 @@ export const toRotatePentominos = writable(0);
 export const toFlipPentominos = writable(0);
 export const pentominosStore = writable([]);
 
+
+export const pArea = writable('');
+export const pIDs = writable('');
+export const pFencedTiles = writable([]);
 export const pyodideSays = writable([]);
